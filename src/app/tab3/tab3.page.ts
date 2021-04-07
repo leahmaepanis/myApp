7cba6ff32b5fef2a5d7691b61645aca6201da16b
AilynAbreu <ailynabreu07@gmail.com>
2021-04-07 10:21:21 +0800

    Commit all files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contName = "";
+  contNumber = "";
+  contacts: any = [];
 
-  constructor() {}
+  constructor(public alertCtrl: AlertController) { }
 
+  saveContact() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+    }
+      this.contacts.push(contact);
+      this.clearField();
+    }
+
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+
+  async showConfirm(cont){
+    const confirm = await this.alertCtrl.create({
+      message: 'Are you sure you want to delete this contact?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Confirm Okay.');
+            let index = this.contacts.indexOf(cont);
+            if(index > -1){
+              this.contacts.splice(index, 1);
+            }
+          }
+        }
+      ]
+    });
+  await confirm.present();
+}
 }
